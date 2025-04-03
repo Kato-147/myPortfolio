ab58180b66657f49a8e4456b3944edd0ee14f961
Nguyễn Viết Toản <kato@MacBook-Pro-cua-Nguyen.local>
2025-04-04 02:36:14 +0700

    update

--- src/content_options.js
+++ src/content_options.js
@@ -12,7 +12,7 @@ const introdata = {
         third: "I develop mobile apps",
     },
     description: "I like code, game, coffe and music",
-    your_img_url: "https://scontent.fsgn5-12.fna.fbcdn.net/v/t39.30808-6/457453390_1905758816570232_625105015124947096_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeE3Ad4ulSo3uIxXpsLWdKMWbF6MqfOMEs5sXoyp84wSztRjkD9tS8YxA0r6ox-e-mmxDWxDyCjPXpPzlg5McGp4&_nc_ohc=aOrzpMzmlZUQ7kNvgHLrcdQ&_nc_ht=scontent.fsgn5-12.fna&oh=00_AYDmNo-t1XiD7vBEdlzMgah_HKE4SA6oydelDzaZb8e38w&oe=66D7AAA5",
+    your_img_url: "https://scontent.fsgn2-9.fna.fbcdn.net/v/t39.30808-6/467294405_1967378463741600_6651304874754369781_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeGvdKxd6a1r05Hg9bmMH0rY-jXMaaj_JHD6NcxpqP8kcBqAmkfYIbn9nGRr_Qm16O99JZZobLEoRAyZoLpZRGNX&_nc_ohc=6w5mMozXDZMQ7kNvwENwjrO&_nc_oc=AdnwAyhUcWRdwTIYhTng1EJhvNQhl8aU8uM-rtaY6Xs6L-NhOU-nbr44vL3F21E9s28&_nc_zt=23&_nc_ht=scontent.fsgn2-9.fna&_nc_gid=YszroBbyWEMbK5RSY8kyxA&oh=00_AYFuMFClVFnlMJNu8giONl2n8aAZQ__x-qraxzGu4ImP6A&oe=67F4C7FC",
 };
 
 const resume ={
