7076154734f49652a97110242579dbddadafa323
kato <oroki147@gmail.com>
2025-04-11 19:00:42 +0700

    update new portfolio

---
+++ components/Header.jsx
@@ -0,0 +1,23 @@
+'use client'
+
+import Link from 'next/link'
+
+const Header = () => {
+  return (
+   <header className="py-8 xl:py-12 text-white">
+    <div className="container mx-auto">
+        {/* logo */}
+        <Link href="/" >
+        <h1 className="text-4xl font-semibold">
+            Kato <span className="text-accent">.</span>
+        </h1>
+        </Link>
+
+        {/* desktop nav */}
+
+    </div>
+   </header>
+  )
+}
+
+export default Header
